f0bd526837a2bcb2186d1045e8d7866c0e81497e
griff і⊙ <346896+griffio@users.noreply.github.com>
2026-02-08 10:52:51 +0000

    project build

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.sqldelight)
+    alias(libs.plugins.flyway)
+    application
+}
+
+group = "griffio"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    google()
+}
+
+dependencies {
+    implementation(libs.sqldelight.jdbc.driver)
+    implementation(libs.postgresql.jdbc.driver)
+    implementation(libs.pgvector)
+    implementation(libs.postgis)
+}
+
+sqldelight {
+    databases {
+        create("Sample") {
+            deriveSchemaFromMigrations.set(true)
+            migrationOutputDirectory = file("$buildDir/generated/migrations")
+            migrationOutputFileFormat = ".sql" // Defaults to .sql
+            packageName.set("griffio.queries")
+            dialect(libs.sqldelight.postgresql.dialect)
+            module("io.github.griffio:sqldelight-postgis:0.0.3")
+            module("io.github.griffio:sqldelight-pgvector:0.0.2")
+        }
+    }
+}
+
+tasks {
+    ///sqldelight task generateMainSampleMigrations will output your .sqm files as valid SQL
+    // in the output directory, with the output format.
+    // Create a dependency from compileKotlin where flyway will have the files available on the classpath
+    compileKotlin.configure {
+        dependsOn("generateMainSampleMigrations")
+    }
+}
+
+flyway {
+    url = "jdbc:postgresql://localhost:5432/geovectors"
+    user = "postgres"
+    password = ""
+    locations = arrayOf("filesystem:$buildDir/generated/migrations")
+    baselineOnMigrate = true
+    baselineVersion = "0"
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("griffio.MainKt")
+}
+
+// https://documentation.red-gate.com/fd/gradle-task-184127407.html
+//:( Without this you may see an error like the following: No database found to handle jdbc:...
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.flyway.database.postgresql)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "sqldelight-postgis-pgvector"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            val vSqlDelight = "2.2.1"
+            plugin("kotlin", "org.jetbrains.kotlin.jvm").version("2.2.21")
+            plugin("sqldelight", "app.cash.sqldelight").version(vSqlDelight)
+            plugin("flyway", "org.flywaydb.flyway").version("10.1.0")
+            library("pgvector", "com.pgvector:pgvector:0.1.6")
+            library("postgis", "net.postgis:postgis-jdbc:2024.1.0")
+            library("sqldelight-jdbc-driver", "app.cash.sqldelight:jdbc-driver:$vSqlDelight")
+            library("sqldelight-postgresql-dialect", "app.cash.sqldelight:postgresql-dialect:$vSqlDelight")
+            library("postgresql-jdbc-driver", "org.postgresql:postgresql:42.5.4")
+            library("flyway-database-postgresql", "org.flywaydb:flyway-database-postgresql:10.1.0")
+            library("google-truth", "com.google.truth:truth:1.4.2")
+        }
+    }
+}
